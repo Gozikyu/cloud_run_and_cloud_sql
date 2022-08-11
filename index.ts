f7683bfe6f98e4badf7e25059699ea448bc496b8
yuta <gozikyu@gmail.com>
2022-08-11 15:29:33 +0900

    setup

---
+++ index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (_req: Request, res: Response) => {
+  return res.status(200).send({
+    message: "Hello World!",
+  });
+});
+
+app.post("/tasks", async (req: Request, res: Response) => {
+  console.info("body:", req.body);
+  return res.status(200).send({
+    message: req.body || "Execute tasks!",
+  });
+});
+
+try {
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`helloworld: listening on port ${port}`);
+  });
+} catch (e) {
+  if (e instanceof Error) {
+    console.error(e.message);
+  }
+}
