1f86de8ab9c83c35a528283bbd3cf7cf6e4ef3d9
yuta <gozikyu@gmail.com>
2022-08-14 14:38:34 +0900

    edit config

--- index.ts
+++ index.ts
@@ -1,13 +1,36 @@
 import express, { Request, Response } from "express";
+import mysql from "mysql";
+import "dotenv";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", async (_req: Request, res: Response) => {
-  return res.status(200).send({
-    message: "Hello World!",
+  // .envファイルに環境変数を記載
+  const connectionName = process.env.CONNECTION_NAME;
+  const dbUser = process.env.DB_USER;
+  const dbPassword = process.env.DB_PASSWORD;
+  const dbName = process.env.DB_NAME;
+
+  const connection = mysql.createConnection({
+    socketPath: "/cloudsql/" + connectionName,
+    user: dbUser,
+    password: dbPassword,
+    database: dbName,
   });
+
+  connection.connect();
+
+  connection.query("SELECT * FROM entries;", (err: any, results: any) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    } else {
+      res.send(JSON.stringify(results));
+    }
+  });
+  connection.end();
 });
 
 app.post("/tasks", async (req: Request, res: Response) => {
